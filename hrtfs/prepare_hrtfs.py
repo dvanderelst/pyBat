4fe65d386a4c1e1c0f5d105d8fc3015410745999
Dieter <vanderdt@ucmail.uc.edu>
2018-08-07 21:40:59 -0400

    Updated Prepare_hrtfs

--- hrtfs/prepare_hrtfs.py
+++ hrtfs/prepare_hrtfs.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot
 import numpy
 import pickle
 import os
-
+import Smoothn
 print(os.getcwd())
 
 ###############################
@@ -20,29 +20,36 @@ freqs = numpy.squeeze(freqs)
 ear_min = numpy.min(left)
 nose_min = numpy.min(nose)
 
-buffer_ear = numpy.ones((73, 36, 141)) * ear_min
-buffer_nose = numpy.ones((73, 36, 141)) * nose_min
 
 right = numpy.fliplr(left)
-
-left = numpy.concatenate((buffer_ear, left, buffer_nose), 1)
-right = numpy.concatenate((buffer_ear, right, buffer_nose), 1)
-nose = numpy.concatenate((buffer_nose, nose, buffer_nose), 1)
-
 mean_right = numpy.mean(right, 2)
 mean_left = numpy.mean(left, 2)
 mean_nose = numpy.mean(nose, 2)
 
+left_side_ear = numpy.reshape(mean_left[:,0],(73,1))
+right_side_ear = numpy.reshape(mean_left[:,72],(73,1))
+left_side_nose = numpy.reshape(mean_nose[:,0],(73,1))
+right_side_nose = numpy.reshape(mean_nose[:,72],(73,1))
+
+a = numpy.repeat(left_side_ear,36, axis=1)
+b = numpy.repeat(right_side_ear,36, axis=1)
+c = numpy.repeat(left_side_nose,36, axis=1)
+d = numpy.repeat(left_side_nose,36, axis=1)
+
+mean_left = numpy.concatenate((a, mean_left, b), 1)
+mean_right = numpy.concatenate((b, mean_right, a), 1)
+mean_nose = numpy.concatenate((c, mean_nose, d), 1)
+
 pyplot.figure()
-pyplot.subplot(1, 3, 1)
+pyplot.subplot(2, 2, 1)
 pyplot.imshow(mean_left)
 pyplot.title('Left')
 
-pyplot.subplot(1, 3, 2)
+pyplot.subplot(2, 2, 2)
 pyplot.imshow(mean_right)
 pyplot.title('Right')
 
-pyplot.subplot(1, 3, 3)
+pyplot.subplot(2, 2, 3)
 pyplot.imshow(mean_nose)
 pyplot.title('Nose')
 
@@ -51,3 +58,4 @@ hrtf = {'left': left, 'right': right, 'nose': nose, 'freq': freqs}
 f = open('pd01.hrtf', 'wb')
 pickle.dump(hrtf, f)
 f.close()
+pyplot.show()
